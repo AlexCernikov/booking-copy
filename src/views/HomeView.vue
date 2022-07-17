de9aa4bc7e92f409bce49e7f40b26df83a408446
Alex <cernikov@mail.ru>
2022-07-17 22:18:14 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HeroSectionComponent msg="Welcome  to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeroSectionComponent from '@/components/HomeViewComponents/HeroSectionComponent.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'heroSection',
+  components: {
+    HeroSectionComponent,
+  },
+});
+</script>
